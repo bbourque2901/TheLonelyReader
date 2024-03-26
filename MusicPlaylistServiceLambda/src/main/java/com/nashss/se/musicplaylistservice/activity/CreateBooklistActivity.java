097be19159337b8b022539e152c1273331896287
Julie <lerchejulie@gmail.com>
2024-03-26 11:22:52 -0500

    beginning create implementation

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/CreateBooklistActivity.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/CreateBooklistActivity.java
@@ -1,4 +1,8 @@
 package com.nashss.se.musicplaylistservice.activity;
-
+/**
+ * Implementation of the CreateBooklistActivity for the MusicPlaylistService's CreateBooklist API.
+ * <p>
+ * This API allows the customer to create a new booklist with no books.
+ */
 public class CreateBooklistActivity {
 }

e890ae77598a67a644d08a7a0e1a5d0cf70a8aa4
Julie <lerchejulie@gmail.com>
2024-03-26 11:56:02 -0500

    tests written and passing

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/CreateBooklistActivity.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/CreateBooklistActivity.java
@@ -2,15 +2,11 @@ package com.nashss.se.musicplaylistservice.activity;
 
 import com.nashss.se.musicplaylistservice.activity.requests.CreateBooklistRequest;
 import com.nashss.se.musicplaylistservice.activity.results.CreateBooklistResult;
-import com.nashss.se.musicplaylistservice.activity.results.CreatePlaylistResult;
-import com.nashss.se.musicplaylistservice.converters.ModelConverter;
 import com.nashss.se.musicplaylistservice.converters.ModelConverterCarbon;
 import com.nashss.se.musicplaylistservice.dynamodb.BooklistDao;
 import com.nashss.se.musicplaylistservice.dynamodb.models.Booklist;
-import com.nashss.se.musicplaylistservice.dynamodb.models.Playlist;
 import com.nashss.se.musicplaylistservice.exceptions.InvalidAttributeValueException;
 import com.nashss.se.musicplaylistservice.models.BooklistModel;
-import com.nashss.se.musicplaylistservice.models.PlaylistModel;
 import com.nashss.se.projectresources.music.playlist.servic.util.MusicPlaylistServiceUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;

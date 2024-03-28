ee7939bcd1a6eebcdda17731a61a226ebf27ad64
Breanna <bourquebreanna@gmail.com>
2024-03-28 09:35:25 -0500

    updated remove booklist functionality to require authenticated request

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/lambda/RemoveBooklistLambda.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/lambda/RemoveBooklistLambda.java
@@ -11,20 +11,22 @@ import org.apache.logging.log4j.Logger;
 
 public class RemoveBooklistLambda
         extends LambdaActivityRunner<RemoveBooklistRequest, RemoveBooklistResult>
-        implements RequestHandler<LambdaRequest<RemoveBooklistRequest>, LambdaResponse> {
+        implements RequestHandler<AuthenticatedLambdaRequest<RemoveBooklistRequest>, LambdaResponse> {
 
     private final Logger log = LogManager.getLogger();
 
     @Override
-    public LambdaResponse handleRequest(LambdaRequest<RemoveBooklistRequest> input, Context context) {
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<RemoveBooklistRequest> input, Context context) {
         log.info("handleRequest");
-        return super.runActivity(
-            () -> input.fromPath(path ->
+        return super.runActivity(() -> {
+            RemoveBooklistRequest unauthenticatedRequest = input.fromBody(RemoveBooklistRequest.class);
+            return input.fromUserClaims(claims ->
                     RemoveBooklistRequest.builder()
-                            .withId(path.get("id"))
-                            .build()),
-            (request, serviceComponent) ->
-                    serviceComponent.provideRemoveBooklistActivity().handleRequest(request)
-        );
+                            .withId(unauthenticatedRequest.getId())
+                            .withCustomerId(claims.get("email"))
+                            .build());
+        }, (request, serviceComponent) ->
+                serviceComponent.provideRemoveBooklistActivity().handleRequest(request));
+
     }
 }

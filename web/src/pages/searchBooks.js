1d6eec4ed4b1f4672ce62d909e78a55b4923289b
acg <acgerhold@gmail.com>
2024-04-05 19:13:38 -0500

    Added thumbnail images to search/view booklist, added placeholder for book progress in view booklist

--- web/src/pages/searchBooks.js
+++ web/src/pages/searchBooks.js
@@ -100,7 +100,7 @@ const EMPTY_DATASTORE_STATE = {
             return '<h4>No results found</h4>';
         }
 
-        let html = '<table><tr><th></th><th>Asin</th><th>Title</th><th>Author</th><th>Genre</th></tr>';
+        let html = '<table><tr><th></th><th>ISBN</th><th>Title</th><th>Author</th><th>Genre</th></tr>';
         for (const res of searchResults) {
             html += `
             <tr>
